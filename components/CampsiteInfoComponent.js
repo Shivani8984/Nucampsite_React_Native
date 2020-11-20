44ba2e56f8e08b7468298b4dd302139e45ce9326
Shivani8984 <shivani8984@gmail.com>
2020-11-20 15:57:34 -0500

    modify render() in campsiteInfo component

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -4,9 +4,10 @@ import { Card } from 'react-native-elements';
 import { CAMPSITES } from '../shared/campsites';
 
 function RenderCampsite({campsite}) {
+
     if (campsite) {
         return (
-            <Card 
+            <Card
                 featuredTitle={campsite.name}
                 image={require('./images/react-lake.jpg')}
             >
@@ -20,21 +21,22 @@ function RenderCampsite({campsite}) {
 }
 
 class CampsiteInfo extends Component {
-    constructor(props){
+
+    constructor(props) {
         super(props);
         this.state = {
             campsites: CAMPSITES
-        }
+        };
     }
 
     static navigationOptions = {
         title: 'Campsite Information'
     }
-    
-    render(){
-        const campsiteId = this.props.naigation.getParam('campsiteId');
+
+    render() {
+        const campsiteId = this.props.navigation.getParam('campsiteId');
         const campsite = this.state.campsites.filter(campsite => campsite.id === campsiteId)[0];
-        return <RenderCampsite campsite={props.campsite} />;
+        return <RenderCampsite campsite={campsite} />;
     }
 }
 
