da7628cb17dac6637f075f286152a5b56ca4cc6d
Shivani8984 <shivani8984@gmail.com>
2020-12-01 12:10:27 -0500

    Animations

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import { View, Text, Animated } from 'react-native';
 import { Card } from 'react-native-elements';
 // import { CAMPSITES } from '../shared/campsites';
 // import { PROMOTIONS } from '../shared/promotions';
@@ -46,14 +46,30 @@ function RenderItem(props) {
 
 class Home extends Component {
 
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         campsites: CAMPSITES,
-    //         promotions: PROMOTIONS,
-    //         partners: PARTNERS
-    //     };
-    // }
+    constructor(props) {
+        super(props);
+        this.state = {
+            // campsites: CAMPSITES,
+            // promotions: PROMOTIONS,
+            // partners: PARTNERS
+            scaleValue: new Animated.Value(0)
+        };
+    }
+
+    animate() {
+        Animated.timing(
+            this.state.scaleValue,
+            {
+                toValue: 1,
+                duration: 1500,
+                useNativeDriver: true
+            }
+        ).start();
+    }
+
+    componentDidMount() {
+        this.animate();
+    }
 
     static navigationOptions = {
         title: 'Home'
@@ -61,7 +77,7 @@ class Home extends Component {
 
     render() {
         return (
-            <ScrollView>
+            <Animated.ScrollView style={{transform: [{scale: this.state.scaleValue}]}}>
                 <RenderItem 
                     item={this.props.campsites.campsites.filter(campsite => campsite.featured)[0]}
                     isLoading={this.props.campsites.isLoading}
@@ -77,7 +93,7 @@ class Home extends Component {
                     isLoading={this.props.partners.isLoading}
                     errMess={this.props.partners.errMess}
                 />
-            </ScrollView>
+            </Animated.ScrollView>
         );
     }
 }
